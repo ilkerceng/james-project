cd11cd65b341406a409c8ad30bcf05ba0ecf28c2
ilker yıldırım <ilker.ceng@gmail.com>
2018-08-23 23:42:26 +0300

    listMailRepository function name is changed as listMailRepositories. and implemented reprocessAllMails, reprocessMail, listMailsInRepository, getMailInRepository, deleteMailInRepository, deleteMailsInRepository

--- server/container/cli/src/main/java/org/apache/james/cli/probe/impl/JMXMailReprocessingProbe.java
+++ server/container/cli/src/main/java/org/apache/james/cli/probe/impl/JMXMailReprocessingProbe.java
@@ -1,10 +1,30 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
 package org.apache.james.cli.probe.impl;
 
 import org.apache.james.adapter.mailbox.MailReprocessingManagementMBean;
 import org.apache.james.mailbox.store.probe.MailReprocessingProbe;
+import org.apache.mailet.Mail;
 
 import javax.management.MalformedObjectNameException;
 import java.io.IOException;
+import java.util.List;
 
 public class JMXMailReprocessingProbe implements MailReprocessingProbe, JmxProbe {
     private static final String MAILREPROCESSINGMANAGEMENT_OBJECT_NAME = "org.apache.james:type=component,name=mailreprocessingmanagementbean";
@@ -32,7 +52,28 @@ public class JMXMailReprocessingProbe implements MailReprocessingProbe, JmxProbe
     }
 
     @Override
-    public String[] listMailRepository() throws Exception {
-        return mailReprocessingManagementMBean.listMailRepository();
+    public List<String> listMailRepositories() throws Exception {
+        return mailReprocessingManagementMBean.listMailRepositories();
     }
+
+    @Override
+    public List<Mail> listMailsInRepository(String repositoryPath) throws Exception {
+        return mailReprocessingManagementMBean.listMailsInRepository(repositoryPath);
+    }
+
+    @Override
+    public Mail getMailInRepository(String repositoryPath, String emailKey) throws Exception {
+        return mailReprocessingManagementMBean.getMailInRepository(repositoryPath, emailKey);
+    }
+
+    @Override
+    public void deleteMailInRepository(String repositoryPath, String emailKey) throws Exception {
+        mailReprocessingManagementMBean.deleteMailInRepository(repositoryPath, emailKey);
+    }
+
+    @Override
+    public void deleteMailsInRepository(String repositoryPath) throws Exception {
+        mailReprocessingManagementMBean.deleteMailsInRepository(repositoryPath);
+    }
+
 }
