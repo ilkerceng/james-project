23ec50a2f1d9dd581fddc033421e4f336e7de87e
ilker yıldırım <ilker.ceng@gmail.com>
2018-08-24 00:07:25 +0300

    added apache mailet module dependency is added

--- mailbox/store/src/main/java/org/apache/james/mailbox/store/probe/MailReprocessingProbe.java
+++ mailbox/store/src/main/java/org/apache/james/mailbox/store/probe/MailReprocessingProbe.java
@@ -1,7 +1,21 @@
 package org.apache.james.mailbox.store.probe;
 
+import org.apache.mailet.Mail;
+
+import java.util.List;
+
 public interface MailReprocessingProbe {
     void reprocessAllMails(String repositoryPath) throws Exception;
+
     void reprocessMail(String repositoryPath, String emailKey) throws Exception;
-    String[] listMailRepository() throws Exception;
+
+    List<String> listMailRepositories() throws Exception;
+
+    List<Mail> listMailsInRepository(String repositoryPath) throws Exception;
+
+    Mail getMailInRepository(String repositoryPath, String emailKey) throws Exception;
+
+    void deleteMailInRepository(String repositoryPath, String emailKey) throws Exception;
+
+    void deleteMailsInRepository(String repositoryPath) throws Exception;
 }
